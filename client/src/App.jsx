c550250dfc7eee08db853b0d3402c35fc9594bd2
N Aishwarya <156683806+aishwaryan23@users.noreply.github.com>
2025-05-18 17:12:51 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -37,11 +37,12 @@ const App = () => {
           <Route path='/cart' element={<Cart/>}/>
           <Route path='/add-address' element={<AddAddress/>}></Route>
           <Route path='/my-orders' element={<MyOrders/>}></Route>
+          <Route path='/loader' element={<Loading/>} />
           <Route path='/seller' element={isSeller ? <SellerLayout/> : <SellerLogin/>}>
           <Route index element={isSeller ? <AddProduct/>:null} />
           <Route path='product-list' element={<ProductList/>} />
           <Route path='orders' element={<Order/>} />
-          <Route path='loader' element={<Loading/>} />
+          
           </Route>
         </Routes>
       </div>
